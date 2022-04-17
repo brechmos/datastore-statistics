b09d837d256a0f336623be281980f39efe07f535
brechmos <craig@imagingAI.org>
2022-04-17 08:58:45 -0400

    changed test to z-score

--- datastore_statistics.py
+++ datastore_statistics.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import argparse
 import numpy as np
 from reader import DataFile
+import scipy.stats as stats
 
 class DataStoreStatistics:
     """
@@ -10,7 +11,10 @@ class DataStoreStatistics:
     def __init__(self, filenames):
         self._filenames = filenames
 
-    def flag_metrics(self, metric='mean', nstds=2, lh='both'):
+        if len(filenames) == 0:
+            raise('No filenames were passed to DataStoreStatistics')
+
+    def flag_metrics(self, metric='mean', nstds=2, lh='both', z_score_threshold=3):
         """
         metric: type of metric to check, needs to match one of metrics from reader get_stats
         nstds: number of standard deviations away from the mean to flag
@@ -23,22 +27,25 @@ class DataStoreStatistics:
 
         mean_values = [reader.get_metrics()[metric] for reader in readers]
 
+        # Compute Z-Score
+        zscores = stats.zscore(mean_values)
+
         mean, std = np.mean(mean_values), np.std(mean_values)
         print(f'Overall mean of metric "{metric}" is {mean:0.1f} and std is {std:0.1f}')
 
-        for reader, mean_value in zip(readers, mean_values):
+        for reader, zscore in zip(readers, zscores):
 
             if lh == 'both':
-                if np.abs(mean_value-mean) > nstds*std:
-                    print(reader)
+                if np.abs(zscore) > z_score_threshold:
+                    print(f'Z-Score {zscore:1.1f}: {reader}')
 
             elif lh == 'lower':
-                if mean_value < mean-nstds*std:
-                    print(reader)
+                if zscore < -z_score_threshold:
+                    print(f'Z-Score {zscore:1.1f}: {reader}')
 
             elif lh == 'higher':
-                if mean_value > mean+nstds*std:
-                    print(reader)
+                if zscore > z_score_threshold:
+                    print(f'Z-Score {zscore:1.1f}: {reader}')
 
 
 if __name__ == '__main__':
@@ -47,11 +54,13 @@ if __name__ == '__main__':
     parser.add_argument("--metric", type=str, dest="metric", help="Metric to check", default='mean')
     parser.add_argument("--nstds", type=int, dest="nstds", help="Number of standard deviations away to flag.", default=2)
     parser.add_argument("--direction", type=str, dest="direction", help="Direction of standard deviations to flag.", default='both')
+    parser.add_argument("--z-score-threshold", type=float, dest="z_score_threshold", help="Threshold on the Z-Score at which to flag.", default=3)
 
     args = parser.parse_args()
 
     print(' ')
 
-    filenames = Path(args.data_folder).glob('*')
+    filenames = list(Path(args.data_folder).glob('*'))
     dss = DataStoreStatistics(filenames)
-    dss.flag_metrics(args.metric, nstds=args.nstds, lh=args.direction)
+    dss.flag_metrics(args.metric, nstds=args.nstds, lh=args.direction, z_score_threshold=args.z_score_threshold)
+
